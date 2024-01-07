9f178b1d475541a18d25542db5bc3aeaba121f61
JacTBB <jackiesoon66@gmail.com>
2024-01-08 03:53:07 +0800

    Client Utility (Free)

--- app/models/Client.py
+++ app/models/Client.py
@@ -1,5 +1,5 @@
 from app.database import db
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Date
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -8,3 +8,12 @@ class Location(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String)
     address: Mapped[str] = mapped_column(String)
+
+class Utility(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    location: Mapped[str] = mapped_column(String)
+    name: Mapped[str] = mapped_column(String)
+    date: Mapped[Date] = mapped_column(Date)
+    carbonfootprint: Mapped[str] = mapped_column(String)
+    energyusage: Mapped[str] = mapped_column(String)
+    waterusage: Mapped[str] = mapped_column(String)
