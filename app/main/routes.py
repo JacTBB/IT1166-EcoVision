48435df7e2961a5bf85e055c00dd76d1114af9b0
seuno <electrokai1@gmail.com>
2024-01-07 21:42:00 +0800

    save post content

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+from flask_login import login_remembered, login_required
 from app.main import main
-from app.database import query_data
+from app.database import query_data, db
 from app.models.News import Post
 from app.main.forms import ArticleForm
-
 
 
 @main.route('/')
@@ -11,19 +11,31 @@ def home():
     return render_template('main/home.html', news=query_data(Post, limit=3))
 
 
-
 @main.route('/services')
 def services():
     return render_template('main/services.html')
 
 
-
-@main.route('/news')
+@main.route('/news', methods=['GET', 'POST'])
 def news():
     query = query_data(Post)
     postid = request.args.get('postid')
     if postid is not None:
         form = ArticleForm()
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                if request.form.get('csrf_token'):
+                    try:
+                        post = Post.query.filter_by(postid=postid).first()
+                        post.title = form.title.data
+                        post.content = form.content.data
+
+                        db.session.add(post)
+                        db.session.commit()
+                    except Exception as e:
+                        print(f"Error occurred: {e}")
+                        db.session.rollback()
+
         query = query_data(Post, filter_by={'postid': postid}, all=False)
         if query is not None:
             return render_template('main/article.html', article=query, form=form)
--- app/models/News.py
+++ app/models/News.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
 
 
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(80))
@@ -14,6 +13,3 @@ class Post(db.Model):
         DateTime, default=datetime.utcnow)
     image_name = db.Column(db.String())
     postid: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
-
-    def __repr__(self) -> str:
-        return f"{self.title} {self.content} {self.author} {self.date} {self.image_name}"
