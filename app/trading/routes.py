8907b10db1a85939139f5d16a293699899eea213
DrPinguJr <poplance88@gmail.com>
2024-01-08 13:36:28 +0800

    carbon T 8 jan

--- app/trading/routes.py
+++ app/trading/routes.py
@@ -7,27 +7,34 @@ from app.trading.forms import AddProjectForm, EditProjectForm
 
 @trading.route('/')
 def home():
-    projects = {1: "project 1", 2: "project 2"}
+    projects = {}
+    projectsData = db.session.query(Projects).all()
+    for project in projectsData:
+        projects[project.id] = {
+            'name': project.name,
+            'type': project.type
+        }
 
     return render_template('trading/Dashboard.html', projects = projects)
 
 
 @trading.route('/project/<project>')
 def project(project):
-    return render_template('trading/ProjectF.html', project = project)
+    projectData = Projects.query.get(project)
+    return render_template('trading/Project.html', project=projectData)
 
-@trading.route("/locations")
+@trading.route("/projects")
 @login_required
-def locations():
-    locations = {}
-    locationsData = db.session.query(Projects).all()
-    for location in locationsData:
-        locations[location.id] = {
-            'name': location.name,
-            'type': location.type
+def projects():
+    projects = {}
+    projectsData = db.session.query(Projects).all()
+    for project in projectsData:
+        projects[project.id] = {
+            'name': project.name,
+            'type': project.type
         }
     
-    return render_template('trading/ProjectF.html', locations=locations)   
+    return render_template('trading/ProjectF.html', projects=projects)   
 
 @trading.route("/project/add", methods=['GET', 'POST'])
 def project_add():
@@ -38,55 +45,55 @@ def project_add():
             name = request.form.get("name")
             type = request.form.get("type")
             
-            location = Projects(name=name, type=type)
-            db.session.add(location)
+            project = Projects(name=name, type=type)
+            db.session.add(project)
             db.session.commit()
             
-            return redirect(url_for('trading.locations'))
+            return redirect(url_for('trading.projects'))
         except Exception as e:
             print(f"Error occurred: {e}")
             db.session.rollback()
     
     return render_template('trading/ProjectA.html', form=form)
 
-@trading.route("/location/<location>/edit", methods=['GET', 'POST'])
+@trading.route("/project/<project>/edit", methods=['GET', 'POST'])
 @login_required
-def location_edit(location):
+def project_edit(project):
     form = EditProjectForm()
     
     if form.validate_on_submit():
         try:
-            locationData = Projects.query.get(location)
+            projectData = Projects.query.get(project)
             
             name = request.form.get("name")
             type = request.form.get("type")
            
             if name:
-                locationData.name = name
+                projectData.name = name
             if type:
-                locationData.type = type
+                projectData.type = type
             
             db.session.commit()
             
-            return redirect(url_for('trading.locations'))
+            return redirect(url_for('trading.projects'))
         except Exception as e:
             print(f"Error occurred: {e}")
             db.session.rollback()
     
     return render_template('trading/ProjectE.html', form=form)
 
-@trading.route("/location/<location>/delete")
+@trading.route("/project/<project>/delete")
 @login_required
-def location_delete(location):
+def project_delete(project):
     try:
-        locationData = Projects.query.get(location)
+        projectData = Projects.query.get(project)
         
-        if locationData is None:
+        if projectData is None:
             return "Projects Not Found!"
         
-        db.session.delete(locationData)
+        db.session.delete(projectData)
         db.session.commit()
-        return redirect(url_for('trading.locations'))
+        return redirect(url_for('trading.projects'))
     except Exception as e:
         print(f"Error occurred: {e}")
         db.session.rollback()
