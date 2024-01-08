bede0ab13f05bc5677171bf2079ba2af808bf342
JacTBB <jackiesoon66@gmail.com>
2024-01-08 14:22:57 +0800

    Users Prototype

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,7 +3,14 @@ from app.auth import auth
 from app.database import db, query_data
 from flask_login import current_user, login_required, login_user, logout_user
 from app.models.User import Client, Author, Technician, Consultant, Manager, Admin
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, AddUserForm, EditUserForm
+
+
+
+UserList = {'client': Client, 'author': Author,
+            'technician': Technician, 'consultant': Consultant,
+            'manager': Manager, 'admin': Admin}
+
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -48,10 +55,96 @@ def login():
     return render_template("auth/login.html", form=form, error_message=error_message)
 
 
+
+@auth.route("/users/<type>")
+@login_required
+def users(type):
+    users = {}
+    usersData = db.session.query(UserList[type]).all()
+    for user in usersData:
+        users[user.id] = {
+            'username': user.username
+        }
+    
+    return render_template('auth/users.html', type=type, users=users)
+
+
+
+@auth.route("/users/<type>/add", methods=['GET', 'POST'])
+@login_required
+def user_add(type):
+    form = AddUserForm()
+    
+    if form.validate_on_submit():
+        try:
+            username = request.form.get("username")
+            
+            user = UserList[type](username=username)
+            user.set_password('123')
+            db.session.add(user)
+            db.session.commit()
+            
+            return redirect(url_for('auth.users', type=type))
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            db.session.rollback()
+    
+    return render_template("auth/users_add.html", form=form)
+
+
+
+# @auth.route("/users/<user>/edit", methods=['GET', 'POST'])
+# @login_required
+# def user_edit(user):
+#     form = EditUserForm()
+    
+#     if request.method == 'POST':
+#         try:
+#             locationData = Location.query.get(location)
+            
+#             name = request.form.get("name")
+#             address = request.form.get("address")
+           
+#             if name:
+#                 locationData.name = name
+#             if address:
+#                 locationData.address = address
+            
+#             db.session.commit()
+            
+#             return redirect(url_for('client.locations'))
+#         except Exception as e:
+#             print(f"Error occurred: {e}")
+#             db.session.rollback()
+    
+#     return render_template("client/location_edit.html", form=form)
+
+
+
+# @auth.route("/users/<user>/delete")
+# @login_required
+# def user_delete(user):
+#     try:
+#         locationData = Location.query.get(location)
+        
+#         if locationData is None:
+#             return "Location Not Found!"
+        
+#         db.session.delete(locationData)
+#         db.session.commit()
+#         return redirect(url_for('client.locations'))
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         db.session.rollback()
+#         return "Error"
+
+
+
 @auth.route("/account")
 @login_required
 def account():
     return render_template("auth/account.html")
+
 
 
 @auth.route('/logout')
