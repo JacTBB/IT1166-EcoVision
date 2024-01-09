af4b798818c770c8a01f853813e3a952083da7e9
seuno <electrokai1@gmail.com>
2024-01-10 07:11:10 +0800

    update serve.py

--- serve.py
+++ serve.py
@@ -1,7 +1,9 @@
-from app import create_app, socketio
 from app.config import Config
+from app import create_app, socketio
+
 
 app = create_app(Config)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=80)
+    socketio.run(app, debug=True, host="0.0.0.0", port=80,
+                 use_reloader=True, log_output=False)
