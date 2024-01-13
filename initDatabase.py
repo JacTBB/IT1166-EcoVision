14c94b32cc0f1a0c7b0ab832b2a1d25fde127569
JacTBB <jackiesoon66@gmail.com>
2024-01-14 03:00:36 +0800

    Updated initDB

--- initDatabase.py
+++ initDatabase.py
@@ -3,12 +3,15 @@ from app.config import Config
 from app.database import db
 from app.models.User import Client, Author, Technician, Consultant, Manager, Admin
 from app.models.News import Post
-from app.models.Contact import CompanyInfo
+from app.models.Client import Location, Utility
+from datetime import date
+from random import randint
 
 app = create_app(Config)
 
 with app.app_context():
 
+    # Users
     UserList = {'client': Client, 'author': Author,
                 'technician': Technician, 'consultant': Consultant,
                 'manager': Manager, 'admin': Admin}
@@ -21,8 +24,10 @@ with app.app_context():
             user.set_company('SomeCompanyName')
 
         db.session.add(user)
-        db.session.commit()
 
+
+
+    # Main
     for i in range(1, 10):
         post = Post(title="test",
                     content="Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consecteteeeur",
@@ -30,4 +35,19 @@ with app.app_context():
                     image_name="Cover-image-1-495x400.jpg",
                     postid=i*2)
         db.session.add(post)
-        db.session.commit()
+        
+    
+    
+    # Client
+    for i in range(1, 3):
+        location = Location(name=f"Office {i}", address="SG 1234")
+        db.session.add(location)
+        
+        for j in range(1, 11):
+            utility = Utility(location=i, name=f"Utility {j}", date=date(2024, j, 1),
+                            carbonfootprint=randint(1,500), energyusage=randint(1,500), waterusage=randint(1,500))
+            db.session.add(utility)
+    
+    
+    
+    db.session.commit()
