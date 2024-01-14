536498fe8b06a820da6d8e07f085d5f80558250e
seuno <electrokai1@gmail.com>
2024-01-14 16:02:44 +0800

    minor changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,22 +4,24 @@ from wtforms.validators import DataRequired
 
 
 class ArticleForm(FlaskForm):
-    image_view_onNews = StringField('Image', validators=[DataRequired()])
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    image_view_onNews = StringField(
+        'Image', validators=[DataRequired()], render_kw={"placeholder": "Image"})
+    title = StringField('Title', validators=[DataRequired()], render_kw={
+                        "placeholder": "Title"})
+    content = TextAreaField('Content', validators=[DataRequired()], render_kw={
+                            "placeholder": "Content"})
     submit = SubmitField('Submit')
 
 
 class ContactForm(FlaskForm):
-    employee_name = StringField('Employee Name', validators=[DataRequired()])
-    company_name = StringField('Company Name', validators=[DataRequired()])
-    company_email = StringField('Company Email', validators=[DataRequired()])
-    industry = StringField('Industry', validators=[DataRequired()])
-    company_size = IntegerField('Company Size', validators=[DataRequired()])
+    employee_name = StringField('Employee Name', validators=[
+                                DataRequired()], render_kw={"placeholder": "Employee Name"})
+    company_name = StringField('Company Name', validators=[
+                               DataRequired()], render_kw={"placeholder": "Company Name"})
+    company_email = StringField('Company Email', validators=[
+                                DataRequired()], render_kw={"placeholder": "Company Email"})
+    industry = StringField('Industry', validators=[DataRequired()], render_kw={
+                           "placeholder": "Industry"})
+    company_size = IntegerField('Company Size', validators=[
+                                DataRequired()], render_kw={"placeholder": "Company Size"})
     submit = SubmitField("Submit")
-
-    # employee_name: Mapped[str] = mapped_column(String)
-    # company_name: Mapped[str] = mapped_column(String)
-    # company_email: Mapped[str] = mapped_column(String)
-    # industry: Mapped[str] = mapped_column(String)
-    # company_size: Mapped[int] = mapped_column(Integer)
