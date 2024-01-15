fa69df7ddf3f2014452ea3c01b1401b8c7858855
seuno <electrokai1@gmail.com>
2024-01-15 12:53:31 +0800

    fix contact page css

--- app/static/js/darkmode.js
+++ app/static/js/darkmode.js
@@ -3,14 +3,15 @@ $(document).ready(function () {
         $(".contact-form").addClass("switch-color");
     }
 
-    var borderColor = $("form input[type='text']").css("border-color");
     $(".toggle-darkmode").click(function () {
         $(".contact-form").toggleClass("switch-color");
 
-        if (borderColor === 'rgb(255, 255, 255)') { // white in rgb
-            $("form input[type='text']").css("border-color", "black");
+        var getBorderColor = $("form input[type='text']").css("border-color")
+
+        if (getBorderColor === 'rgb(255, 255, 255)') { // white in rgb
+            $("form input[type='text'], form input[type='number']").css("border-color", "black");
         } else {
-            $("form input[type='text']").css("border-color", "white");
+            $("form input[type='text'], form input[type='number']").css("border-color", "white");
         }
     });
 });
