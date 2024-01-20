e6a5e573a8e8be285f69fab8575b7683b8af435b
seuno <electrokai1@gmail.com>
2024-01-20 20:01:01 +0800

    home clients logos

--- app/main/routes.py
+++ app/main/routes.py
@@ -25,7 +25,7 @@ from string import ascii_lowercase, ascii_uppercase
 @main.route('/')
 def home():
 
-    return render_template('main/home.html', news=Post.query.order_by(desc(Post.postid)).all())
+    return render_template('main/home.html', news=Post.query.order_by(desc(Post.postid)).limit(4))
 
 
 @main.route('/services')
