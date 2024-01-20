12a70a4a2958c53e57c65d9e9e66869eaf53bef7
seuno <electrokai1@gmail.com>
2024-01-20 12:44:01 +0800

    fix edward's code

--- app/models/User.py
+++ app/models/User.py
@@ -6,42 +6,41 @@ from flask_login import UserMixin
 from flask_bcrypt import Bcrypt
 
 
-
 bcrypt = Bcrypt()
-
 
 
 class User(db.Model, UserMixin):
     __abstract__ = True
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[str] = mapped_column(String, unique=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String)
     type: Mapped[str] = mapped_column(String)
-    
+
     def __init__(self, username, type):
         self.user_id = str(uuid.uuid4())
         self.username = username
         self.type = type
-    
+
     def get_id(self):
         return self.user_id
 
     def set_password(self, password: str) -> None:
-        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password_hash = bcrypt.generate_password_hash(
+            password).decode('utf-8')
 
     def check_password(self, password: str) -> bool:
         return bcrypt.check_password_hash(self.password_hash, password)
 
 
-
 class Client(User):
-    company: Mapped[int] = mapped_column(Integer)
-    
-    def __init__(self, username, company):
+    company: Mapped[str] = mapped_column(String)
+
+    def __init__(self, username):
         super().__init__(username, 'client')
-    
-    def set_company(self, company: int):
+
+    def set_company(self, company: str):
         self.company = company
 
     def get_company(self):
@@ -52,16 +51,22 @@ class Author(User):
     def __init__(self, username):
         super().__init__(username, 'author')
 
+
 class Technician(User):
     def __init__(self, username):
         super().__init__(username, 'technician')
+
+
 class Consultant(User):
     def __init__(self, username):
         super().__init__(username, 'consultant')
 
+
 class Manager(User):
     def __init__(self, username):
         super().__init__(username, 'manager')
+
+
 class Admin(User):
     def __init__(self, username):
         super().__init__(username, 'admin')
